208e8f85007a153fcbe634df8e35e755607be63b
rtanoeiro <ramontanoeiro@gmail.com>
2025-02-16 16:01:18 +1100

    moved window to another file

--- main.py
+++ main.py
@@ -1,37 +1,21 @@
-from tkinter import Tk, BOTH, Canvas
-from shapes import Line, Point
-
-
-class Window:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.root_widget = Tk(screenName="Maze Solver")
-        self.canvas_widget = Canvas(self.root_widget, height=height, width=width)
-        self.canvas_widget.pack(fill=BOTH, expand=True)
-        self.is_running = False
-
-    def redraw(self):
-        self.root_widget.update_idletasks()
-        self.root_widget.update()
-
-    def wait_for_close(self):
-        self.is_running = True
-        while self.is_running:
-            self.redraw()
-
-    def draw_line(self, line: Line, fill_color: str):
-        line.draw(canvas=self.canvas_widget, fill_color=fill_color)
-
-    def close(self):
-        self.is_running = False
-        self.root_widget.destroy()
+from window import Window
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    win.draw_line(Line(point0=Point(25, 25), point1=Point(50, 50)), fill_color="red")
-    win.draw_line(Line(point0=Point(50, 25), point1=Point(25, 50)), fill_color="red")
+    cell = Cell(
+        has_left_wall=True,
+        has_right_wall=True,
+        has_top_wall=True,
+        has_bottom_wall=True,
+        _x1=1,
+        _y1=1,
+        _x2=100,
+        _y2=100,
+        _win=win,
+    )
+    cell.draw()
     win.wait_for_close()
 
 
---
+++ window.py
@@ -0,0 +1,28 @@
+from tkinter import Tk, BOTH, Canvas
+from shapes import Line
+
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.root_widget = Tk(screenName="Maze Solver")
+        self.canvas_widget = Canvas(self.root_widget, height=height, width=width)
+        self.canvas_widget.pack(fill=BOTH, expand=True)
+        self.is_running = False
+
+    def redraw(self):
+        self.root_widget.update_idletasks()
+        self.root_widget.update()
+
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(canvas=self.canvas_widget, fill_color=fill_color)
+
+    def close(self):
+        self.is_running = False
+        self.root_widget.destroy()
