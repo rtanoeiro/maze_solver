7317bdc5a1508dfa1f430255070d58966f90d00e
rtanoeiro <ramontanoeiro@gmail.com>
2025-02-16 17:32:26 +1100

    added tests

---
+++ tests.py
@@ -0,0 +1,34 @@
+import unittest
+from cell import Maze
+
+
+class Tests(unittest.TestCase):
+    def test_maze_create_cells(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertEqual(
+            len(m1.cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1.cells[0]),
+            num_rows,
+        )
+
+    def test_maze_create_cells_2(self):
+        num_cols = 8
+        num_rows = 6
+        m1 = Maze(0, 0, num_rows, num_cols, 8, 6)
+        self.assertEqual(
+            len(m1.cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1.cells[0]),
+            num_rows,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
