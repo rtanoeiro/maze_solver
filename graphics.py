1e3e69ecaccd5e21c2086540ec8427c1009a4f61
rtanoeiro <ramontanoeiro@gmail.com>
2025-02-16 16:54:47 +1100

    moved window and shapes to graphics

--- graphics.py
+++ graphics.py
@@ -1,5 +1,21 @@
-from tkinter import Tk, BOTH, Canvas
-from shapes import Line
+from tkinter import BOTH, Canvas, Tk
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point0: Point, point1: Point):
+        self.point0 = point0
+        self.point1 = point1
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.point0.x, self.point0.y, self.point1.x, self.point1.y, fill=fill_color, width=5
+        )
 
 
 class Window:
@@ -7,7 +23,9 @@ class Window:
         self.width = width
         self.height = height
         self.root_widget = Tk(screenName="Maze Solver")
-        self.canvas_widget = Canvas(self.root_widget, height=height, width=width)
+        self.canvas_widget = Canvas(
+            self.root_widget, height=height, width=width, background="white"
+        )
         self.canvas_widget.pack(fill=BOTH, expand=True)
         self.is_running = False
 
